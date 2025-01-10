424a324cf6f8d786699cb9b08c87be849416b22e
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-10 14:51:23 +0300

    Add project files.

---
+++ Practice_Constructor/Baby.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Constructor
+{
+    internal class Baby
+    {
+        public Baby()
+        {
+
+        }
+
+        public Baby(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public DateTime DateOfBirth;
+        public string Name;
+        public string Surname;               
+
+        public void Introduce()
+        {
+            Console.WriteLine(Name + " " + Surname + ": Ingaaaa");            
+            Console.WriteLine("Date of Birth: " + DateOfBirth);
+        }
+
+    }
+}
---
+++ Practice_Constructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using Practice_Constructor;
+
+Baby baby1 = new Baby()
+{
+    DateOfBirth = DateTime.Now,
+    Name = "Baby",
+    Surname = "One"
+};
+
+
+Baby baby2 = new Baby("Baby", "Two")
+{
+    DateOfBirth = DateTime.Now
+};
+
+baby1.Introduce();
+Console.WriteLine("-------------------------------------");
+baby2.Introduce();
